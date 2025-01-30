fbe4f3f018eeb540d3223085ce07138a494aa195
a1851026 <a1851026@adelaide.edu.au>
2025-01-30 11:21:54 +1030

    Tweaked dialogue

--- ConversationManager.cs
+++ ConversationManager.cs
@@ -12,17 +12,22 @@ public class ConversationManager : MonoBehaviour
 
     public float characterDelay; 
     public float lineDelay;
+    public float disapearDelay;
+    public float initialDelay;
     float characterDelayTracked = 0;
 
+    bool isActive = false;
     bool playingSequence = false;
 
     String currentLine;
     public TextMeshProUGUI outputLine;
 
+    public RectTransform background;
+
 
 
     void Start(){
-
+        HideText();
     }
 
     void Update(){
@@ -34,19 +39,26 @@ public class ConversationManager : MonoBehaviour
             charIndex = 0;
             currentLine = "";
             playingSequence = true;
-            characterDelayTracked = 0;
+            characterDelayTracked = -initialDelay;
             
         }
         else{
             characterDelayTracked += Time.deltaTime;
 
+            
+
+
             if(characterDelayTracked > characterDelay){
+                isActive = true;
+                ShowText();
 
                 characterDelayTracked = 0;
 
                 AudioManager.Singleton.Play("WalkieDialogue");
 
                 Step();
+            } else if (characterDelayTracked > -lineDelay + disapearDelay && !isActive) {
+                HideText();
             }
         }
     }
@@ -58,6 +70,8 @@ public class ConversationManager : MonoBehaviour
         }
 
         currentLine += activeSequence.lines[lineIndex].bursts[burstIndex].content[charIndex];
+
+        
 
         charIndex++;
 
@@ -71,6 +85,7 @@ public class ConversationManager : MonoBehaviour
                 burstIndex = 0;
                 lineIndex++;
                 characterDelayTracked -= lineDelay;
+                isActive = false;
 
                 if(lineIndex >= activeSequence.lines.Length){
                     playingSequence = false;
@@ -82,12 +97,21 @@ public class ConversationManager : MonoBehaviour
             }
         }
 
-
-
         outputLine.text = currentLine;
+
+        Vector2 textSize = outputLine.GetRenderedValues(true);
+        background.sizeDelta = new Vector2(textSize.x + 50f, textSize.y + 30f);
     }
 
+    private void HideText() {
+        background.gameObject.SetActive(false);
+        outputLine.gameObject.SetActive(false);
+    }
 
+    private void ShowText() {
+        background.gameObject.SetActive(true);
+        outputLine.gameObject.SetActive(true);
+    }
 
 
 }
